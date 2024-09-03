4982ae27adaba1ff796bcffe1d212698e53fe391
tokslaw-shruboak <147814066+tokslaw-shruboak@users.noreply.github.com>
2024-09-03 15:23:28 -0400

    PopularJobs component added & APIcall folder added

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from 'react'
+import {View, Text, TouchableOpacity, FlatList, ActivityIndicator, ActivityIndicatorComponent} from 'react-native'
 
 import styles from './popularjobs.style'
+import  { COLORS, SIZES} from "../../../constants";
+import PopularJobCard from "../../common/cards/popular/PopularJobCard";
+import {useRouter} from "expo-router";
 
 const Popularjobs = () => {
+    const router = useRouter();
+    const isLoading = false;
+    const error = false;
+
   return (
-    <View>
-      <Text>Popularjobs</Text>
+    <View style={styles.container}>
+        <View style={styles.header}>
+      <Text style={styles.headerTitle}>Popular jobs ok</Text>
+            <TouchableOpacity>
+                <Text style={styles.headerBtn}>Show all</Text>
+            </TouchableOpacity>
+    </View>
+        <View style={styles.cardsContainer}>
+            {isLoading ? (
+                <ActivityIndicator size="large" colors={ COLORS.primary} />
+            ) : error ? (
+                <Text>Something went wrong</Text>
+            ) : (
+                <FlatList
+                data={[1,2,3,4,5]}
+                renderItem={({ item }) =>(
+                    <PopularJobCard
+                        item={ item }
+                    />
+                )}
+                keyExtractor={item => item?.job_id}
+                contentContainerStyle={{ columnGap: SIZES.medium }}
+                />
+            )}
+        </View>
     </View>
   )
 }
